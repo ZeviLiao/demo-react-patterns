bb90bd8d2874803447f0d41751dd5b98192db6d0
Zevi Liao <jointjm@gmail.com>
2019-06-06 02:19:17 +0800

    render props

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 
+
+class Toggle extends React.Component {
+  static defaultProps = { onToggle: () => { } }
+  state = { on: false }
+  toggle = () =>
+    this.setState(
+      ({ on }) => ({ on: !on }),
+      () => this.props.onToggle(this.state.on),
+    )
+  render() {
+    return this.props.children({ on: this.state.on, toggle: this.toggle });
+  }
+}
+
+
+
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        Home
-      </div>
+      <Toggle
+        onToggle={on => console.log('toggle', on)}
+      >
+        {({ on, toggle }) => (
+          <div>
+            {on ? 'The button is on' : 'The button is off'}
+            <hr />
+            <button onClick={toggle}>
+              {on ? 'click on' : 'click off'}
+            </button>
+          </div>
+        )}
+      </Toggle>
     )
   }
 }
